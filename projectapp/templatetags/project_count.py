b91e531a72f60b854bf9718349dd8e69b12938e2
bello <belloyusuf1997@gmail.com>
2022-01-07 05:04:25 +0000

    second commit on the site

--- projectapp/templatetags/project_count.py
+++ projectapp/templatetags/project_count.py
@@ -1,9 +1,10 @@
 from django import template
-from ..models import Project
+from ..models import Project, Category
 
 register = template.Library()
 
 @register.simple_tag()
 def total_project():
     return Project.objects.filter(available=True).count()
-    
+
+
