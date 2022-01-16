96b58e624ac8743dfb7c89b5eb0f05c38e7424ac
bello <belloyusuf1997@gmail.com>
2022-01-16 17:00:44 +0000

    Done with user login remain user registration.

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,27 @@
 from django import forms
 from django.forms.widgets import EmailInput, FileInput, PasswordInput, Select, SelectMultiple, TextInput, Textarea
+from django.contrib.auth.models import User
+
+
+
+# User Registration form
+class UserRegistrationForm(forms.ModelForm):
+    """ User Registration form """
+    password = forms.CharField(max_length=6, required=True, 
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=6, required=True,
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email', 'image')
+        
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password did\'t match')
+        return cd['password2']
+
 
 # User login form
 class LoginForm(forms.Form):
