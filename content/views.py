81c50d996b414d18438e14821d4b3892b9bd3e3e
bello <belloyusuf1997@gmail.com>
2022-01-24 23:06:36 +0000

    done with system project detail.

--- content/views.py
+++ content/views.py
@@ -79,7 +79,6 @@ def purchaseProject(request):
                 phone = cd['phone'],
                 upload_image=cd['upload_image'])
             user_purchase.save()
-            messages.add_message(request, messages.SUCCESS, "Your project topic would be sent to you via your Email Address")
             return redirect('/content/done/')
     else:
         form = ProjectPurchase()
@@ -94,6 +93,7 @@ def purchaseDone(request):
     project = Project.objects.filter(available=True)
     category = Category.objects.all()
     user_info = Purchase.objects.all()
+    messages.add_message(request, messages.SUCCESS, "Your project topic would be sent to you via your Email Address")
     return render(request, 
                   'content/purchase_done.html', 
                   {'user_info':user_info,
